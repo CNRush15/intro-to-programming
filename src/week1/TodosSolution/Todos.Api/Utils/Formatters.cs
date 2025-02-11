823b96023e04bb62f339fa9f76205dcd0838ade5
CNRush15 <cnrush15@gmail.com>
2025-02-11 17:23:46 +0000

    TestingCode

---
+++ src/week1/TodosSolution/Todos.Api/Utils/Formatters.cs
@@ -0,0 +1,9 @@
+﻿namespace Todos.Api.Utils;
+
+public class Formatters
+{
+    public string FormatName(string firstName, string lastName)
+    {
+        return firstName + " " + lastName;
+    }
+}
---
+++ src/week1/TodosSolution/Todos.Tests/UnitTest1.cs
@@ -0,0 +1,32 @@
+﻿using Todos.Api.Utils;
+
+namespace Todos.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void CanFormatAName()
+    {
+        //given
+        string firstName = "Bob", lastName = "Smith", fullName;
+        var formatter = new Formatters();
+        //when
+        fullName= formatter.FormatName(firstName, lastName);
+        //then
+        Assert.Equal("Bob Smith", fullName);
+        
+
+    }
+
+    [Theory]
+    [InlineData("Bob", "Smith", "Bob Smith")]
+    [InlineData("Luke", "Skywalker", "Luke Skywalker")]
+    
+    public void CanFormatAnyName(string firstName, string lastName, string expecting)
+    {
+        var formatter = new Formatters();
+        var fullName = formatter.FormatName(firstName, lastName);   
+
+        Assert.Equal(expecting, fullName);
+    }
+}
