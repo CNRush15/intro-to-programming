547c67831bb63db487fa12063591bb6e5595be59
CNRush15 <cnrush15@gmail.com>
2025-02-12 21:10:43 +0000

    First 4 Tests Passing

---
+++ src/week1/string-calculator-weds/StringCalculator/Calculator.cs
@@ -0,0 +1,20 @@
+﻿public class Calculator
+{
+    public int Add(string numbers)
+    {
+        if (string.IsNullOrEmpty(numbers))
+        {
+            return 0;
+        }
+
+        var numberArray = numbers.Split(',');
+        int sum = 0;
+
+        foreach (var number in numberArray)
+        {
+            sum += int.Parse(number);
+        }
+
+        return sum;
+    }
+}
---
+++ src/week1/string-calculator-weds/StringCalculator/CalculatorTests.cs
@@ -0,0 +1,50 @@
+﻿namespace StringCalculator;
+public class CalculatorTests
+{
+    [Fact]
+    public void EmptyStringReturnsZero()
+    {
+        var calculator = new Calculator();
+
+        var result = calculator.Add("");
+
+        Assert.Equal(0, result);
+    }
+
+    [Theory]
+    [InlineData("1", 1)]
+    [InlineData("2", 2)]
+    [InlineData("10", 10)]
+    public void SingleDigitReturnsInteger(string numbers, int expected)
+    {
+        var calculator = new Calculator();
+
+        var result = calculator.Add(numbers);
+
+        Assert.Equal(expected, result);
+    }
+
+    [Theory]
+    [InlineData("1,2", 3)]
+    [InlineData("3,4", 7)]
+    public void TwoNumbersSeparatedByCommaReturnsSum(string numbers, int expected)
+    {
+        var calculator = new Calculator();
+
+        var result = calculator.Add(numbers);
+
+        Assert.Equal(expected, result);
+    }
+
+    [Theory]
+    [InlineData("1,2,3", 6)]
+    [InlineData("1,2,3,4,5,6,7,8,9", 45)]
+    public void ArbitraryLengthStringReturnsSum(string numbers, int expected)
+    {
+        var calculator = new Calculator();
+
+        var result = calculator.Add(numbers);
+
+        Assert.Equal(expected, result);
+    }
+}
