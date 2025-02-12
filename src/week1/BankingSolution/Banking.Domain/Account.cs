0a3479523924086fe0e083e466c3d7c3facc14a3
CNRush15 <cnrush15@gmail.com>
2025-02-12 20:19:09 +0000

    Added Banking

---
+++ src/week1/BankingSolution/Banking.Domain/Account.cs
@@ -0,0 +1,17 @@
+﻿namespace Banking.Domain
+{
+    public class Account
+    {
+        private decimal _openingBalance = 5000;
+
+        public void Deposit(decimal amountToDeposit)
+        {
+            _openingBalance += amountToDeposit;
+        }
+
+        public decimal GetBalance()
+        {
+            return _openingBalance;
+        }
+    }
+}
---
+++ src/week1/BankingSolution/Banking.Tests/Syntax/Types.cs
@@ -0,0 +1,7 @@
+﻿
+namespace Banking.Tests.Syntax
+{
+    public class Types
+    {
+    }
+}
