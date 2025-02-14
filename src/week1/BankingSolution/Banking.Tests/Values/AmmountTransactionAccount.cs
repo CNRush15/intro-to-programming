505862c70b4ee1c3b082fd472437c1cb05ac9561
CNRush15 <cnrush15@gmail.com>
2025-02-14 17:08:52 +0000

    updates to banking

---
+++ src/week1/BankingSolution/Banking.Tests/Values/AmmountTransactionAccount.cs
@@ -0,0 +1,31 @@
+﻿
+
+using Banking.Domain;
+
+namespace Banking.Tests.Values;
+public class AccountTransactionAmountTests
+{
+    [Fact]
+    public void ThrowsOnNegativeAmount()
+    {
+        Assert.Throws<AccountNegativeTransactionAmountException>(() => new AccountTransactionAmount(-3));
+    }
+
+    [Fact]
+    public void ThrowsOnAmountsOverThreshold()
+    {
+        Assert.Throws<AccountTranactionAmountBeyondLimits>(() => new AccountTransactionAmount(10_000.01M));
+    }
+
+    [Fact]
+    public void CanGetValue()
+    {
+        var amountToDeposit = new AccountTransactionAmount(232.55M);
+
+        decimal balance = amountToDeposit;
+
+        AccountTransactionAmount yourPay = 12.23M;
+
+        Assert.Equal<decimal>(232.55M, amountToDeposit);
+    }
+}
