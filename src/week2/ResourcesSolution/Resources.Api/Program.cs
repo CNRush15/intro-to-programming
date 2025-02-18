3908b50529bf49a3f88d1bbc0473440de5a0616b
CNRush15 <cnrush15@gmail.com>
2025-02-18 19:17:43 +0000

    update

--- src/week2/ResourcesSolution/Resources.Api/Program.cs
+++ src/week2/ResourcesSolution/Resources.Api/Program.cs
@@ -6,8 +6,20 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+builder.Services.AddCors(options =>
+{
+    // classroom demonstration - probably ok, but check with your local authorities. ;)
+    options.AddDefaultPolicy(pol =>
+    {
+        pol.AllowAnyHeader();
+        pol.WithMethods();
+        pol.AllowAnyOrigin();
+    });
+});
+
 var app = builder.Build();
 
+app.UseCors();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
