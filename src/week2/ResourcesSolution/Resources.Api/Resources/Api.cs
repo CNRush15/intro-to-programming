01ca887c6cd2ff101d8f81d636d28a1cdba679d8
CNRush15 <cnrush15@gmail.com>
2025-02-18 19:00:45 +0000

    Updates after lunch

---
+++ src/week2/ResourcesSolution/Resources.Api/Resources/Api.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Resources.Api.Resources;
+
+public class Api : ControllerBase
+{
+    [HttpGet("/resources")]
+    public async Task<ActionResult> GetAllResources()
+    {
+        return Ok();
+    }
+}
